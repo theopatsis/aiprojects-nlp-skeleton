f9ae641b3be9ac41a9c35d69326d57db1c336157
wz-ml <willzhou97@ucla.edu>
2023-02-03 19:38:03 -0800

    See changelog -> primary changes = oversampling underrepresented class.

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -1,7 +1,7 @@
 import torch
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-
+import numpy as np
 
 class StartingDataset(torch.utils.data.Dataset):
     """
@@ -21,8 +21,19 @@ class StartingDataset(torch.utils.data.Dataset):
         self.vectorizer = CountVectorizer(stop_words='english', max_df=0.99, min_df=0.005)
         self.sequences = self.vectorizer.fit_transform(self.df.question_text.tolist()) # matrix of word counts for each sample
         self.labels = self.df.target.tolist() # list of labels
+        print(self.sequences.shape)
+        self.data_df = self.oversample(pd.DataFrame({"data": self.sequences.toarray().tolist(),
+                        "target": self.labels}))
+        self.sequences = np.stack(self.data_df["data"].values, axis = 0); self.labels = self.data_df["target"].tolist()
+        print(self.sequences.shape)
         self.token2idx = self.vectorizer.vocabulary_ # dictionary converting words to their counts
         self.idx2token = {idx: token for token, idx in self.token2idx.items()} # same dictionary backwards
+
+    def oversample(self, df):
+        sincere = df[df["target"] == 0].copy()
+        insincere = df[df["target"] == 1].copy()
+        upsampled = insincere.sample(len(sincere.index), axis=0, replace = True, ignore_index = True)
+        return pd.concat([sincere, upsampled], axis=0, ignore_index = True)
 
     # TODO: return an instance from the dataset
     def __getitem__(self, i):
@@ -30,7 +41,8 @@ class StartingDataset(torch.utils.data.Dataset):
         i (int): the desired instance of the dataset
         '''
         # return the ith sample's list of word counts and label
-        return self.sequences[i, :].toarray(), self.labels[i]
+
+        return self.sequences[i, :], self.labels[i]
 
     # TODO: return the size of the datasetfloat(
     def __len__(self):
--- split_train.py
+++ split_train.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
+def upsample_insincere(df):
+    sincere = df[df["target"] == 0].copy()
+    insincere = df[df["target"] == 1].copy()
+    upsampled = insincere.sample(len(sincere.index), axis=0, replace = True, ignore_index = True)
+    return pd.concat([sincere, upsampled], axis=0, ignore_index = True)
+
 # read DataFrame
-data = pd.read_csv("train.csv")
+data = pd.read_csv("datasets/train.csv")
 
 # no of csv files with row size
 k = 2
@@ -11,7 +17,9 @@ size_val = 261216 + size_train
 print(data.shape)
 
 df = data[0:size_train]
+#sampled_df = upsample_insincere(df)
 df.to_csv(f'train_80.csv', index=False)
 
 df = data[size_train:size_val]
+#ampled_df = upsample_insincere(df)
 df.to_csv(f'train_val_20.csv', index=False)
